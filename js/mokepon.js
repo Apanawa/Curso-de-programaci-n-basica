f58867ba0ffd9a6e8d9cbe412a7d4f0e54d80ca6
Adan Gonzalez Flores <ehecatl16@hotmail.com>
2024-06-02 20:02:57 -0600

    Funciones de ataque

--- js/mokepon.js
+++ js/mokepon.js
@@ -1,6 +1,15 @@
+let ataqueJugador 
+
 function iniciarJuego() {
     let botonMascotaJugador = document.getElementById("boton-mascota")
     botonMascotaJugador.addEventListener("click", seleccionarMascotaJugador)
+
+    let botonFuego = document.getElementById("boton-fuego")
+    botonFuego.addEventListener("click", ataqueFuego)
+    let botonAgua = document.getElementById("boton-agua")
+    botonAgua.addEventListener("click", ataqueAgua)
+    let botonTierra = document.getElementById("boton-tierra")
+    botonTierra.addEventListener("click", ataqueTierra)
 }
 
 function seleccionarMascotaJugador() {
@@ -35,6 +44,20 @@ function seleccionarMascotaEnemigo() {
     }
 }
 
+function ataqueFuego(){
+    ataqueJugador = "FUEGO"
+    alert(ataqueJugador)
+}
+
+function ataqueAgua(){
+    ataqueJugador = "AGUA"
+    alert(ataqueJugador)
+}
+
+function ataqueTierra(){
+    ataqueJugador = "TIERRA"
+    alert(ataqueJugador)
+}
 
 function aleatorio(min,max) {   
     return Math.floor(Math.random() * (max - min + 1) + min)

833500896eed1a4984f54b340a186d719ebbc3ca
Apanawa <ehecatl16@hotmail.com>
2024-06-02 23:07:23 -0600

    resultado de ataque

--- js/mokepon.js
+++ js/mokepon.js
@@ -72,14 +72,24 @@ function ataqueAleatorioEnemigo(){
         ataqueEnemigo = "TIERRA"
     } 
 
-    crearMensaje()
+    combate()
 }
 
-function crearMensaje(){
+function combate(){
+    if(ataqueEnemigo == ataqueJugador){
+        crearMensaje("EMPATE")
+    } else if(ataqueJugador == "FUEGO" && ataqueEnemigo == "TIERRA" || ataqueJugador == "AGUA" && ataqueEnemigo == "FUEGO" || ataqueJugador == "TIERRA" && ataqueEnemigo == "AGUA") {
+        crearMensaje("GANASTE")
+    } else {
+        crearMensaje("PERDISTE")
+    }
+}
+
+function crearMensaje(resultado){
     let sectionMensajes = document.getElementById("mensajes")
 
     let parrafo = document.createElement("p")
-    parrafo.innerHTML = "Tu mascota atacó con " + ataqueJugador + ", la mascota del enemigo atacó con " + ataqueEnemigo + "- PENDIENTE 🏆"
+    parrafo.innerHTML = "Tu mascota atacó con " + ataqueJugador + ", la mascota del enemigo atacó con " + ataqueEnemigo + " - " + resultado
 
     sectionMensajes.appendChild(parrafo)
 }

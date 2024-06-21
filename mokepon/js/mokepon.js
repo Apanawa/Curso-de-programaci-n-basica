55ecf39052abf2577c47616becc977b28dcf0894
Apanawa <ehecatl16@hotmail.com>
2024-06-21 00:44:20 -0600

    77 JSON y POST

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -21,6 +21,7 @@ const contenedorAtaques = document.getElementById('contenedorAtaques')
 const sectionVerMapa = document.getElementById("ver-mapa")
 const mapa = document.getElementById("mapa")
 
+let jugadorId = null
 let mokepones = []
 let ataqueJugador =[]
 let ataqueEnemigo = []
@@ -186,6 +187,7 @@ function unirseAlJuego() {
             res.text()
                 .then(function (respuesta){
                     console.log(respuesta)
+                    jugadorId = respuesta
                 })
         }
     })
@@ -195,7 +197,6 @@ function seleccionarMascotaJugador() {
     
     sectionSeleccionarMascota.style.display = 'none'
 
-    
     if (inputHipodoge.checked) {
         spanMascotaJugador.innerHTML = inputHipodoge.id
         mascotaJugador = inputHipodoge.id
@@ -209,10 +210,23 @@ function seleccionarMascotaJugador() {
         alert('Selecciona una mascota')
     }
 
+    seleccionarMokepon(mascotaJugador)
+
     extraerAtaques(mascotaJugador)
     sectionVerMapa.style.display = "flex"
     iniciarMapa ()
-    
+}
+
+function seleccionarMokepon (mascotaJugador) {
+    fetch(`http://localhost:8080/mokepon/${jugadorId}`, {
+        method : "post",
+        headers : {
+            "Content-Type" : "application/json"
+        },
+        body: JSON.stringify({
+            mokepon: mascotaJugador
+        })
+    })
 }
 
 function extraerAtaques(mascotaJugador) {
